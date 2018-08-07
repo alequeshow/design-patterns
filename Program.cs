1b4ff25deae11e9f7a344593f7cc423191395868
Alex Cardoso de Almeida <acalmeida@trendfeiras.com.br>
2018-08-07 14:36:21 -0300

    Including Prototype basic sample

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DesignPatterns.Builder;
 using DesignPatterns.Factory;
+using DesignPatterns.Prototype;
 
 namespace DesignPatterns
 {
@@ -9,7 +10,8 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
             //DemoBuilder();
-            DemoFactory();
+            //DemoFactory();
+            DemoPrototype();
         }
 
         static void DemoBuilder()
@@ -28,6 +30,24 @@ namespace DesignPatterns
 
             Console.WriteLine(person1);
             Console.WriteLine(person2);
+        }
+
+        static void DemoPrototype()
+        {
+            var ln1 = new Line();
+            ln1.Start = new Point(){X = 12, Y = 6};
+            ln1.End = new Point(){X = 60, Y = 22};
+
+            var ln2 = ln1.DeepCopy();
+
+            ln2.Start.X = 6;
+            ln2.Start.Y = 12;
+
+            ln2.End.X = 22;
+            ln2.End.Y = 60;
+
+            Console.WriteLine($"Line 1 Start at ({ln1.Start.X},{ln1.Start.Y}) and ends at ({ln1.End.X},{ln1.End.Y})");
+            Console.WriteLine($"Line 2 Start at ({ln2.Start.X},{ln2.Start.Y}) and ends at ({ln2.End.X},{ln2.End.Y})");
         }
     }
 }
---
+++ Prototype/Line.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace DesignPatterns.Prototype
+{
+    public class Point
+    {
+      public int X, Y;
+    }
+
+    public class Line
+    {
+      public Point Start, End;
+
+      //Not the best approach but fulfills what asked in the exercise
+      public Line DeepCopy()
+      {
+        return new Line()
+        {
+            Start = new Point()
+            {
+                X = this.Start.X,
+                Y = this.Start.Y
+            },
+            End = new Point()
+            {
+                X = this.End.X,
+                Y = this.End.Y
+            }
+        };
+      }
+    } 
+}
